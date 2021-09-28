f795428f1a01449a5f09e86b525ffaafc9b9c075
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-29 00:20:57 +0300

    add Author

---
+++ src/main/java/com/example/backend/model/Author.java
@@ -0,0 +1,26 @@
+package com.example.backend.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "dop_info", nullable = true)
+    private String dop_info;
+
+}
